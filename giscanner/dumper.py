c6489e975bf86f841f57da8261959b875522b514
Justin Chadwell <justin.chadwell@pexip.com>
2021-10-04 10:28:27 +0100

    pexhack: allow gobject-instrospection to accept and use -f flags
    Annoyingly, -f flags can modify the compilation and linking of programs,
    specifically in the case of -fsanitize options to provide the
    {Address,Memory,Thread,UndefinedBehavior}Sanitzer. Unfortunately, while
    in some cases we can *hack* around this with -lasan (and variations),
    this only works when the sanitizer is dynamically linked, and is still
    not even properly supported in those cases!
    
    This hack allows reading the -f options from the commandline, and
    passing them in with the rest of the CFLAGS and LDFLAGS - this provides
    better support for sanitizers at this level.
    
    Ideally, in the future we can get this patch upstream, since it's
    reasonably minimally invasive, and provides important compatibility for
    sanitizers.

--- giscanner/dumper.py
+++ giscanner/dumper.py
@@ -187,6 +187,7 @@ class DumpCompiler(object):
         cflags = pkgconfig.cflags(self._packages,
                                   msvc_syntax=self._compiler.check_is_msvc())
         cflags.extend(self._options.cflags)
+        cflags.extend(["-f" + flag for flag in self._options.fflags])
         return self._compiler.compile(cflags,
                                       self._options.cpp_includes,
                                       sources,
@@ -268,6 +269,8 @@ class DumpCompiler(object):
 
         dll_dirs = utils.dll_dirs()
         dll_dirs.add_dll_dirs(self._packages)
+
+        args.extend(["-f" + flag for flag in self._options.fflags])
 
         if not self._options.quiet:
             print("g-ir-scanner: link: %s" % (
